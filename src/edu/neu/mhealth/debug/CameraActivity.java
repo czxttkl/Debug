7e446528e7202b8c5ca648d0a152be149f1ddd77
czxttkl <czxttkl@gmail.com>
2013-11-10 10:42:34 -0500

    reliable version of hough line detection

--- src/edu/neu/mhealth/debug/CameraActivity.java
+++ src/edu/neu/mhealth/debug/CameraActivity.java
@@ -7,7 +7,11 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import edu.neu.mhealth.debug.helper.Global;
 
@@ -22,8 +26,17 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 
 	/*Basic Variables*/
 	private final String TAG = Global.APP_LOG_TAG;
+	private final int ACCUMULATOR_THRESHOLD = 50;
+	private final int MINLINELENGTH = 100;
+	private final int MAXLINEGAP = 80;
+	private final int HYSTERESIS_THRESHOLD1 = 50;
+	private final int HYSTERESIS_THRESHOLD2 = 400;
+	private final int HOUGH_LINE_COUNT = 5;
 	
 	/*OpenCv Variables*/
+	private Mat mRgba;
+	private Mat mGray;
+	private Mat lines;
 	private CameraBridgeViewBase mOpenCvCameraView;
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 		@Override
@@ -86,8 +99,7 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 	 *   */
 	@Override
 	public void onCameraViewStarted(int width, int height) {
-		// TODO Auto-generated method stub
-		
+		lines = new Mat();
 	}
 
 	@Override
@@ -98,7 +110,27 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-		return inputFrame.rgba();
+		mRgba = inputFrame.rgba();
+		mGray = inputFrame.gray();
+		Mat cannyMat = new Mat();
+		mGray.submat(1, mGray.rows()-1, 1, mGray.cols()-1).copyTo(cannyMat);
+		Imgproc.Canny(mGray, cannyMat, HYSTERESIS_THRESHOLD1, HYSTERESIS_THRESHOLD2, 3, false);
+		Imgproc.HoughLinesP(cannyMat, lines, 1, Math.PI/180, ACCUMULATOR_THRESHOLD, MINLINELENGTH, MAXLINEGAP);
+		
+		for (int x = 0; x < lines.cols() && x < HOUGH_LINE_COUNT; x++) {
+	          double[] vec = lines.get(0, x);
+	          if(vec!=null) {
+	        	  double x1 = vec[0], 
+	        			  y1 = vec[1],
+	        			  x2 = vec[2],
+	        			  y2 = vec[3];
+	        	  Point start = new Point(x1, y1);
+	        	  Point end = new Point(x2, y2);
+	        	  Core.line(mRgba, start, end, new Scalar(255,0,0), 3);
+	          }
+	    }
+		
+		return mRgba;
 	}
 
 	
